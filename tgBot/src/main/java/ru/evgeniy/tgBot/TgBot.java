10bc8acc2913e55cc3eb7a488a100a221d5559fc
Evgen <eugene.grinchenko@yandex.com>
2024-07-03 10:46:01 +0300

    Практическое задание 2.

---
+++ tgBot/src/main/java/ru/evgeniy/tgBot/TgBot.java
@@ -0,0 +1,13 @@
+package ru.evgeniy.tgBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgBot.class, args);
+	}
+
+}
---
+++ tgBot/src/test/java/ru/evgeniy/tgBot/TgBotTests.java
@@ -0,0 +1,13 @@
+package ru.evgeniy.tgBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
